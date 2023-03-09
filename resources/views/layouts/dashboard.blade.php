78991465b341d48accb29acc05c4491904deeed2
ShahidulLaravel <shahidul.webdev@gmail.com>
2023-03-09 19:05:32 +0600

    gitignore update

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -66,10 +66,13 @@
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
               <a class="dropdown-item" href="{{route('edit.profile')}}">Edit Profile</a>
               <a class="dropdown-item" href="#">Settings</a>
+
               <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();" >Logout</a>
+
               <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
               @csrf
              </form>
+             
             </div>
 
           </li>
